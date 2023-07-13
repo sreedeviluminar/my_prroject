e1ba15b50a66b501e8b8848ac97c6a0f3d835f8f
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-07-13 12:15:22 +0530

    initial commit

--- lib/firebase exaples/storage/firbase_file_storage.dart
+++ lib/firebase exaples/storage/firbase_file_storage.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
@@ -10,110 +10,133 @@ import 'package:path/path.dart' as path;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(
-    home: FireBase_Storeee(),
+  runApp(const MaterialApp(
+    home: FireHome(),
   ));
 }
 
-class FireBase_Storeee extends StatefulWidget {
+class FireHome extends StatefulWidget {
+  const FireHome({Key? key}) : super(key: key);
+
   @override
-  State<FireBase_Storeee> createState() => _FireBase_StoreeeState();
+  State<FireHome> createState() => _FireHomeState();
 }
 
-class _FireBase_StoreeeState extends State<FireBase_Storeee> {
+class _FireHomeState extends State<FireHome> {
   FirebaseStorage storage = FirebaseStorage.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Image Store"),
+        title: const Text("FireBase Storage"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(12),
+        padding: const EdgeInsets.all(15),
         child: Column(
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton.icon(
-                    onPressed: () => uploadData('camera'),
+                    onPressed: () => upload('camera'),
                     icon: const Icon(Icons.camera),
                     label: const Text("Camera")),
                 ElevatedButton.icon(
-                    onPressed: () => uploadData('gallery'),
+                    onPressed: () => upload('galley'),
                     icon: const Icon(Icons.photo_library),
                     label: const Text("Gallery")),
               ],
             ),
             Expanded(
                 child: FutureBuilder(
-                    future: loadData(),
-                    builder: (context,
-                        AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        return ListView.builder(
-                            itemCount: snapshot.data?.length ?? 0,
-                            itemBuilder: (context, index) {
-                              final images = snapshot.data![index];
-                              return Card(
-                                child: ListTile(
-                                  leading: Image.network(images['url']),
-                                  title: Text(images['description']),
-                                  subtitle: Text(images['uploaded_by']),
-                                  trailing: IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(Icons.delete)),
+                  future: loadData(),
+                  builder: (context,
+                      AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return ListView.builder(
+                          itemCount: snapshot.data?.length ?? 0,
+                          itemBuilder: (context, index) {
+                            final Map<String, dynamic> image = snapshot.data![index];
+                            return Card(
+                              child: ListTile(
+                                leading: Image.network(image['url']),
+                                title:  Text(image['uploaded_by']),
+                                subtitle:  Text(image['description']),
+                                trailing: IconButton(
+                                  onPressed: ()=>delete(image['path']),
+                                  icon: const Icon(Icons.delete),
                                 ),
-                              );
-                            });
-                      }
-                      return const Center(child: CircularProgressIndicator());
-                    }))
+                              ),
+                            );
+                          });
+                    }
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                ))
           ],
         ),
       ),
     );
   }
 
- Future<List<Map<String,dynamic>>> loadData() async{
-   List<Map<String,dynamic>> images =[];
-
-   final ListResult result = await storage.ref().list();
-   final List<Reference> allfiles = result.items;
-   await Future.forEach(allfiles, (sfile) async{
-     final String url = await sfile.getDownloadURL();
-     final FullMetadata metadata = await sfile.getMetadata();
-
-     images.add({
-       'url': url,
-       'path': sfile.fullPath,
-       'uploaded_by':metadata.customMetadata?['uploaded_by']?? "Nobody",
-       'description':metadata.customMetadata?['description']??"Nodescription"
-     });
-   });
-   return images;
- }
-
-  Future<void> uploadData(String imagepath) async {
+  Future<void> upload(String imageip) async {
     final picker = ImagePicker();
     XFile? pickedImage;
     try {
       pickedImage = await picker.pickImage(
-          source: imagepath == 'camera' ? ImageSource.camera : ImageSource.gallery, maxWidth: 1920);
+          source:
+          imageip == 'camera' ? ImageSource.camera : ImageSource.gallery,
+          maxWidth: 1920);
       final String fileName = path.basename(pickedImage!.path);
-      File imageFile = File(pickedImage.path);
+      File imagefile = File(pickedImage.path);
       try {
-        await storage.ref(fileName).putFile(imageFile,
-            SettableMetadata(customMetadata:
-            {'description': "Its me Xxx",
-              'uploaded_by':'Some Description'
+        //upload to firebase
+        await storage.ref(fileName).putFile(
+            imagefile,
+            SettableMetadata(customMetadata: {
+              "uploaded_by": 'Its me xxxxxx',
+              'description': 'Some description....'
             }));
-      }on FirebaseException catch (error) {
-        print(error);
+        //refresh ui
+        setState(() {});
+      } on FirebaseException catch (error) {
+        if (kDebugMode) {
+          print(error);
+        }
       }
-    } catch(e){
-      print('Exception $e');
+    } catch (err) {
+      if (kDebugMode) {
+        print(err);
+      }
     }
+  }
+
+  Future<List<Map<String, dynamic>>> loadData() async {
+    List<Map<String, dynamic>> images = [];
+
+    final ListResult result = await storage.ref().list();
+    final List<Reference> allfiles = result.items;
+
+    await Future.forEach(allfiles, (sfile) async {
+      final String fileUrl = await sfile.getDownloadURL();
+      final FullMetadata fileMeta = await sfile.getMetadata();
+
+      images.add({
+        'url': fileUrl,
+        'path': sfile.fullPath,
+        'uploaded_by': fileMeta.customMetadata?['uploaded_by'] ?? 'NoBody',
+        'description':
+        fileMeta.customMetadata?['description'] ?? 'No Description'
+      });
+    });
+    return images;
+  }
+
+  Future<void> delete(String imagepath) async{
+    await storage.ref(imagepath).delete();
+    setState(() {});
   }
 }
